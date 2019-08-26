209c94b06e2295aaace5c52caadcacfefa2ca004
khaled2252 <khaled.khaleddahmed@gmail.com>
2019-08-26 19:57:57 +0200

    Removed unused variables

--- src/biggerisgreater/BiggerIsGreater.java
+++ src/biggerisgreater/BiggerIsGreater.java
@@ -21,41 +21,36 @@ public class BiggerIsGreater {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a string");
         char[] string = scanner.next().toCharArray();
-        
-         int n = string.length - 1;
-        char temp = 0;
-        int replacedCharIndex = -1;
-        int toBeReplacedCharIndex = -1;
-        int minimumCharIndex = -1;
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = n; j > i; j--) {
-                if (string[j] > string[i]) {
-                    if (toBeReplacedCharIndex == -1) {
-                        toBeReplacedCharIndex = i;
-                        minimumCharIndex = j;
 
-                    } else {
-                        if (string[minimumCharIndex] > string[j]) {
+        int n = string.length - 1;
+        int i;
+        int minimumCharIndex = -1;
+
+        for (i = n - 1; i >= 0; i--) { //decrements from 2nd last element to the first element
+            for (int j = n; j > i; j--) { //decrements from the last element to i
+                if (string[j] > string[i]) {
+                    if (minimumCharIndex==-1) { //first minimum char
+                        minimumCharIndex = j;
+                    } else { //compares each new character with the minumum
+                        if (string[minimumCharIndex] >= string[j]) {
                             minimumCharIndex = j;
                         }
                     }
-
                 }
             }
-            if (minimumCharIndex > -1) {
-                temp = string[minimumCharIndex];
-                string[minimumCharIndex] = string[toBeReplacedCharIndex];
-                string[toBeReplacedCharIndex] = temp;
-                replacedCharIndex = toBeReplacedCharIndex;
+            if (minimumCharIndex > -1) {//Swaps if found at least one character greater than i
+                char temp = string[minimumCharIndex];
+                string[minimumCharIndex] = string[i];
+                string[i] = temp;
                 break;
             }
         }
-        if (replacedCharIndex == -1) {
+        if (minimumCharIndex == -1) {
             System.out.println("No answer");
-        } else {
-            Arrays.sort(string, replacedCharIndex + 1, string.length);
+        } else {//Sort rest of the array after swapping 
+            Arrays.sort(string, i + 1, string.length);
             System.out.println(string);
 
         }
-
+    }
 }
